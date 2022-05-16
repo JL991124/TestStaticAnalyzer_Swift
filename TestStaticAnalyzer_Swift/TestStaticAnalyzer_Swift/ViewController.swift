5e39bee8a5e6fba4fd44a5e710a1a8149ef4bdda
jay.lam <jay.lam@bbpos.com>
2022-05-16 12:19:36 +0800

    1

--- TestStaticAnalyzer_Swift/TestStaticAnalyzer_Swift/ViewController.swift
+++ TestStaticAnalyzer_Swift/TestStaticAnalyzer_Swift/ViewController.swift
@@ -12,32 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        var isloop1 = true
-        var isloop2 = true
-        var i = 1
-        while isloop1 {
-            i += 1
-            if i < 1 {
-                isloop2 = false
-            }
-            print(i)
-        }
-        while isloop2 {
-            print(i)
-            isloop1 = false
-        }
-        
-//        var row:Int = 0
-//        var max:Int = 10
-//        for row in 0...max {
-//            max+=1
-//            print(row)
-//
-//            for row in 0...row+1{
-//                print(row)
-//            }
-//        }
     }
 
 
