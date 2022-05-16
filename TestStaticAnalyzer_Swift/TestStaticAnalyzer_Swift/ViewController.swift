2116469d253b181255e4edb3a85abd0f9183e981
jay.lam <jay.lam@bbpos.com>
2022-05-16 16:13:52 +0800

    1613

--- TestStaticAnalyzer_Swift/TestStaticAnalyzer_Swift/ViewController.swift
+++ TestStaticAnalyzer_Swift/TestStaticAnalyzer_Swift/ViewController.swift
@@ -12,17 +12,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        var i = 0
-        var isLoop = true
-        while isLoop {
-            i+=1
-            if i < 0 {
-                isLoop = false
+        foreverLoop()
+        
+    }
+    
+    func intToString(_ temp: Int) -> String {
+        var tempString: String? = nil
+        switch temp {
+        case 0:
+            tempString = "0"
+        case 1:
+            tempString = "1"
+        case 2:
+            tempString = "2"
+        default:
+            break
+        }
+        let resultString = tempString
+        return resultString!
+    }
+    
+    func foreverLoop() {
+        var row = 0
+        var column = 0
+        var value = 0
+        
+        for row in 0..<3 {
+            column = 0
+            while column <= row {
+                value += 1
+                NSLog("\(value)")
             }
-            print(i)
         }
     }
-
-
 }
+
+
 
